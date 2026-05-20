60e738d75ed234595db97032b418bab2c3f9e065
Your Name <you@example.com>
2026-05-20 22:17:17 +0000

    hdhhdh

--- src/controllers/BrandController.java
+++ src/controllers/BrandController.java
@@ -4,20 +4,21 @@ import models.Brand;
 
 public class BrandController {
     public Brand[] sortSelectionAsc(Brand[] brands) {
-        int n=brands.length;
-        for (int i =0; i<n-1;i++){
-            int indicemin=i;
-            for (int j=i+1; j<n;j++){
-                if (brands[indicemin].getTotalValidYears() > brands[j].getTotalValidYears()){
-                    Brand aux=brands[j];
-                    brands[j]=brands[indicemin];
-                    brands[indicemin]=aux;
+        int n = brands.length;
+        for (int i = 0; i < n - 1; i++) {
+            int indicemin = i;
+            for (int j = i + 1; j < n; j++) {
+                if (brands[indicemin].getTotalValidYears() > brands[j].getTotalValidYears()) {
+                    Brand aux = brands[j];
+                    brands[j] = brands[indicemin];
+                    brands[indicemin] = aux;
                 }
             }
 
         }
         return brands;
     }
+
     public Brand binarySearchByValidYears(Brand[] brands, int validYears, boolean isAscending) {
         int inicio = 0;
         int fin = brands.length - 1;
@@ -27,27 +28,17 @@ public class BrandController {
             int anosMedio = brands[medio].getTotalValidYears();
 
             if (anosMedio == validYears) {
-                return brands[medio]; // Éxito: Encontró la marca
+                return brands[medio];
             }
-
-            if (isAscending) {
-                // Lógica para orden ascendente
-                if (anosMedio > validYears) {
-                    fin = medio - 1;
-                } else {
-                    inicio = medio + 1;
-                }
+            if (anosMedio > validYears) {
+                fin = anosMedio - 1;
             } else {
-                // Lógica para orden descendente
-                if (anosMedio > validYears) {
-                    inicio = medio + 1; // Completado por ti en el paso anterior
-                } else {
-                    fin = medio - 1; // Completado por ti en el paso anterior
-                }
+                inicio = anosMedio + 1;
             }
-        }
-        return null; // No se encontró la marca
-    }
-}
-  
 
+        }
+        return null;
+
+    }
+
+}
